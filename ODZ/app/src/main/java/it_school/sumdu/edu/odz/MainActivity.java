6e51157323d66d0d011bbb57ad4e291f575618d8
Dmytro Molchanov <dima-25@ukr.net>
2023-05-27 19:51:03 +0300

    Add registration and authorization pages

--- ODZ/app/src/main/java/it_school/sumdu/edu/odz/MainActivity.java
+++ ODZ/app/src/main/java/it_school/sumdu/edu/odz/MainActivity.java
@@ -1,8 +1,12 @@
 package it_school.sumdu.edu.odz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            setContentView(R.layout.authorization);
+        }, 3000);
     }
+
+
 }
