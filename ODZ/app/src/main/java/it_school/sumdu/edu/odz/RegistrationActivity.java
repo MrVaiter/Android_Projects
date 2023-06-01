6b197590ac082fdc63fa78a355208a926eb987f9
Dmytro Molchanov <dima-25@ukr.net>
2023-06-01 22:28:14 +0300

    Add back button to registration

--- ODZ/app/src/main/java/it_school/sumdu/edu/odz/RegistrationActivity.java
+++ ODZ/app/src/main/java/it_school/sumdu/edu/odz/RegistrationActivity.java
@@ -19,10 +19,19 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
         db = new DatabaseHelper(getApplicationContext());
 
         findViewById(R.id.submitBtn).setOnClickListener(this);
+        findViewById(R.id.backBtn).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+
+        // Back to authorization
+        if(view.getId() == R.id.backBtn){
+            Intent intent = new Intent(RegistrationActivity.this, AuthorizationActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
 
         Integer ID = db.getNewUserId();
         String login = ((EditText) findViewById(R.id.loginInput)).getText().toString();
@@ -30,16 +39,19 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
         String password = ((EditText) findViewById(R.id.passInput)).getText().toString();
         String repPassword = ((EditText) findViewById(R.id.repeatPassInput)).getText().toString();
 
+        // Login unique check
         if(db.getAccount(login).getCount() != 0){
             Toast.makeText(getApplicationContext(), "Login already exists", Toast.LENGTH_SHORT).show();
             return;
         }
 
+        // Repeat password check
         if(!password.equals(repPassword)){
             Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_SHORT).show();
             return;
         }
 
+        // Email format check
         if(!email.contains("@")){
             Toast.makeText(getApplicationContext(), "Wrong email format", Toast.LENGTH_SHORT).show();
             return;
@@ -47,6 +59,7 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
 
         Boolean result = db.insertUserData(ID, login, email, password);
 
+        // Success check
         if(result){
             Intent intent = new Intent(RegistrationActivity.this, AuthorizationActivity.class);
             startActivity(intent);
