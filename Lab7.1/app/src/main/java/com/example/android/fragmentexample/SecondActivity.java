a9364cc9c74bfc507a5e7f04a641e97493a724ea
Dmytro Molchanov <dima-25@ukr.net>
2023-06-04 19:20:11 +0300

    Finish lab7

--- Lab7.1/app/src/main/java/com/example/android/fragmentexample/SecondActivity.java
+++ Lab7.1/app/src/main/java/com/example/android/fragmentexample/SecondActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2017 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.android.fragmentexample;
 
@@ -24,10 +9,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     private Button mButton;
-    private Button nextButton;
+    private Button previousButton;
     private boolean isFragmentDisplayed = false;
 
     // Saved instance state key.
@@ -36,10 +21,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_activity);
         // Get the button for opening and closing the fragment.
         mButton = findViewById(R.id.open_button);
-        nextButton = findViewById(R.id.next_button);
+
+        previousButton = findViewById(R.id.previous_button);
+
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToFirstActivity();
+            }
+        });
+
 
         // If returning from a configuration change, get the
         // fragment state and set the button text.
@@ -61,18 +55,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToSecondActivity();
-            }
-        });
-
     }
 
-    public void goToSecondActivity(){
-        Intent intent = new Intent(this, SecondActivity.class);
+    public void goToFirstActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
